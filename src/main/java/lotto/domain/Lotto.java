821d202fd50dc4ad567a3bd1abd743b8161d9653
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-10-31 02:15:16 +0900

    feat: Lotto 검증 과정 세분화

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.util.ErrorMessage;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,9 +12,36 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
+    }
+
+    // TODO : 상수값 리팩토링 필요 (로또 규칙에서 할당)
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(ErrorMessage.UNMATCH_WINNING_AMOUNT.getMessage());
         }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        long numberSize = numbers.size();
+        long numberUniqueSize = numbers.stream().distinct().count();
+
+        if (numberSize != numberUniqueSize) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_LOTTO_NUMBER.getMessage());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        int LOWER_BOUND = 1;
+        int UPPER_BOUND = 45;
+
+        boolean outOfRange = numbers.stream()
+                .anyMatch(number -> (number < LOWER_BOUND) || (number > UPPER_BOUND));
+
+        if (outOfRange)
+            throw new IllegalArgumentException(ErrorMessage.INVALID_RANGE_NUMBER.getMessage());
     }
 
     // TODO: 추가 기능 구현

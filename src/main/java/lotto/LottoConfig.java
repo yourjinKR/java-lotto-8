9c49e954689c30b864610b5f659d8f6be9af4106
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-01 03:16:32 +0900

    feat: LottoConfig에 기능 요구사항에 맞는 객체 생성 메서드 추가

--- src/main/java/lotto/LottoConfig.java
+++ src/main/java/lotto/LottoConfig.java
@@ -1,8 +1,41 @@
 package lotto;
 
+import java.util.List;
+import lotto.domain.LottoPickRule;
+import lotto.domain.LottoRule;
+import lotto.domain.Winning;
+
 public class LottoConfig {
     public static final int START_INCLUSIVE = 1;
     public static final int END_INCLUSIVE = 45;
     public static final int COUNT = 6;
     public static final int PRICE = 1_000;
+
+    // 로또 규칙 세팅
+    public static LottoRule getDefaultLottoRule() {
+        return new LottoRule(
+                PRICE,
+                getDefaultLottoPickRule(),
+                getDefaultWinningRule()
+        );
+    }
+
+    // 추첨 규칙 세팅
+    public static LottoPickRule getDefaultLottoPickRule() {
+        return new LottoPickRule(
+                LottoConfig.START_INCLUSIVE,
+                LottoConfig.END_INCLUSIVE,
+                LottoConfig.COUNT);
+    }
+
+    // 당첨 기준과 상금을 아래에 세팅
+    public static List<Winning> getDefaultWinningRule() {
+        return List.of(
+                new Winning(3, false, 5_000),
+                new Winning(4, false, 50_000),
+                new Winning(5, false, 1_500_000),
+                new Winning(5, true, 30_000_000),
+                new Winning(6, true, 2_000_000_000)
+        );
+    }
 }

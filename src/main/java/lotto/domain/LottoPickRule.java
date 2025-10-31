92adcaa7564d20c02fcf732d793bbb603d4d71cd
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-01 01:23:38 +0900

    feat: 당첨규칙 객체 생성시 검증 추가

--- src/main/java/lotto/domain/LottoPickRule.java
+++ src/main/java/lotto/domain/LottoPickRule.java
@@ -2,6 +2,7 @@ package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
+import lotto.util.ErrorMessage;
 
 public class LottoPickRule implements PickRule<List<Integer>> {
     private final int startInclusive;
@@ -9,9 +10,17 @@ public class LottoPickRule implements PickRule<List<Integer>> {
     private final int count;
 
     public LottoPickRule(int startInclusive, int endInclusive, int count) {
+        validate(startInclusive, endInclusive, count);
         this.startInclusive = startInclusive;
         this.endInclusive = endInclusive;
         this.count = count;
+    }
+
+    private void validate(int startInclusive, int endInclusive, int count) {
+        int gap = endInclusive - startInclusive;
+        if (gap+1 < count) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_RANGE_NUMBER.getMessage());
+        }
     }
 
     @Override

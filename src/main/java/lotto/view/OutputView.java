6c7c8c41ea04e593e2ab42d426e2fa230eb86030
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-01 03:40:14 +0900

    feat: 로또 발행 결과 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,21 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.dto.LottoCreateResponse;
+
 public class OutputView {
     public OutputView() {}
+
+    public void printBill(List<LottoCreateResponse> responseList) {
+        int amount = responseList.size();
+        System.out.printf("%d개를 구매했습니다.", amount);
+        LottoView.nexLine();
+        responseList.forEach(this::printLottoNumbers);
+        LottoView.nexLine();
+    }
+
+    private void printLottoNumbers(LottoCreateResponse response) {
+        List<Integer> numbers = response.numbers();
+        System.out.println(numbers);
+    }
 }

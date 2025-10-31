14fb7f24ad0795e3f76a2a244576d4ee8f969a28
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-01 03:23:57 +0900

    feat: repository에 당첨규칙 객체 기본 생성 메서드 삭제

--- src/main/java/lotto/repository/LottoRepository.java
+++ src/main/java/lotto/repository/LottoRepository.java
@@ -5,19 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.Lotto;
 import lotto.domain.PickRule;
-import lotto.domain.Winning;
 
 public class LottoRepository {
-
-    public List<Winning> getDefaultWinningRule() {
-        return List.of(
-                new Winning(3, false, 5_000),
-                new Winning(4, false, 50_000),
-                new Winning(5, false, 1_500_000),
-                new Winning(5, true, 30_000_000),
-                new Winning(6, true, 2_000_000_000)
-        );
-    }
 
     public List<Lotto> registerAsAmountByRule(int amount, PickRule<List<Integer>> pickRule) {
         List<Lotto> lottoList = new ArrayList<>();

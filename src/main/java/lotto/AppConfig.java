71d5b157f938094675ab71b252cf085a45df57b0
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-01 03:44:03 +0900

    feat: 의존성 관리 수정 Service에 Repository와 Mapper를 주입 후 Controller에 View와 Service 주입

--- src/main/java/lotto/AppConfig.java
+++ src/main/java/lotto/AppConfig.java
@@ -1,14 +1,18 @@
 package lotto;
 
 import lotto.controller.LottoController;
+import lotto.mapper.LottoMapper;
+import lotto.repository.LottoRepository;
+import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
 public class AppConfig {
-    public InputView inputView = new InputView();
-    public OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final LottoService lottoService = new LottoService(new LottoRepository(), new LottoMapper());
 
-    public LottoController lottoController = new LottoController(inputView, outputView);
+    private final LottoController lottoController = new LottoController(inputView, outputView, lottoService);
 
     public LottoController lottoController() {
         return lottoController;

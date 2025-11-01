5da7b94712bf825e5dc309e4a7280f288af6be60
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-02 00:37:33 +0900

    feat: 당첨 로또번호 객체

---
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -0,0 +1,25 @@
+package lotto.domain;
+
+import java.util.List;
+import lotto.LottoConfig;
+import lotto.util.ErrorMessage;
+
+public class WinningLotto extends Lotto implements HasBonusNumber<Integer> {
+    private final int bonsNumber;
+
+    public WinningLotto(List<Integer> numbers, int bonsNumber) {
+        super(numbers);
+        this.bonsNumber = bonsNumber;
+    }
+
+    @Override
+    public Integer getBonusNumber() {
+        return bonsNumber;
+    }
+
+    private void validateBonusNumber(int bonsNumber) {
+        if ((bonsNumber < LottoConfig.START_INCLUSIVE) || (bonsNumber > LottoConfig.END_INCLUSIVE)) {
+            throw new IllegalArgumentException(ErrorMessage.OUT_OF_RANGE_NUMBER.getMessage());
+        }
+    }
+}

1286e412ce43e746e01118922ba8fe8f020cedb8
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-01 21:42:34 +0900

    test: 매칭점수와 보너스 매칭 여부가 일치하다면 매칭된 카운트 증가 메서드 테스트

--- src/test/java/lotto/domain/WinningTest.java
+++ src/test/java/lotto/domain/WinningTest.java
@@ -2,15 +2,22 @@ package lotto.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class WinningTest {
+    Winning winning;
+
+    @BeforeEach
+    void setUp() {
+        winning = new Winning(3, false, 5000);
+    }
 
     @Test
     @DisplayName("매칭되는 조건이면 true")
     void isMatchedTrue() {
-        Winning winning = new Winning(3, false, 5000);
         assertThat(winning.isMatched(3, false))
                 .isTrue();
     }
@@ -18,8 +25,29 @@ class WinningTest {
     @Test
     @DisplayName("매칭되는 조건이면 false")
     void isMatchedFalse() {
-        Winning winning = new Winning(3, false, 5000);
         assertThat(winning.isMatched(4, false))
                 .isFalse();
+    }
+
+    @Test
+    @DisplayName("매칭된거라면 카운트가 증가")
+    void countUpMatchedAmountTest() {
+        winning.countUpIfMatched(3, false);
+        List<Winning> list = List.of(winning);
+
+        assertThat(list)
+                .extracting("matchedCount")
+                .containsOnly(1);
+    }
+
+    @Test
+    @DisplayName("매칭된거라면 카운트가 증가하지 않음")
+    void countUpMatchedAmountFailTest() {
+        winning.countUpIfMatched(6, true);
+        List<Winning> list = List.of(winning);
+
+        assertThat(list)
+                .extracting("matchedCount")
+                .containsOnly(0);
     }
 }

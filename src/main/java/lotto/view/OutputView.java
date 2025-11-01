940d86b1b8dc2d8197a2dcb5595d828d6b8945a0
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-01 23:53:50 +0900

    feat(OutputView): 로또 결과 출력 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,10 @@
 package lotto.view;
 
+import java.text.MessageFormat;
 import java.util.List;
 import lotto.controller.dto.PurchaseResponse;
+import lotto.controller.dto.ResultResponse;
+import lotto.controller.dto.WinningResponse;
 
 public class OutputView {
     public OutputView() {}
@@ -17,5 +20,38 @@ public class OutputView {
     private void printLottoNumbers(PurchaseResponse response) {
         List<Integer> numbers = response.numbers();
         System.out.println(numbers);
+    }
+
+    public void printLottoResult(ResultResponse response) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+
+        List<WinningResponse> winningResponseList = response.winningResponseList();
+        winningResponseList.forEach(this::printWinningResult);
+
+        String yield = MessageFormat.format("{0}%입니다.", response.yield());
+        System.out.println();
+    }
+
+    private void printWinningResult(WinningResponse response) {
+        String bonusMention = bonusMention(response.bonus());
+
+        String result = MessageFormat.format(
+                "{0}개 일치{1} ({2}) - {3}개",
+                response.score(),
+                bonusMention,
+                response.prizeMoney(),
+                response.matchedAmount()
+        );
+
+        System.out.println(result);
+    }
+
+    private String bonusMention(boolean bonus) {
+        if (bonus) {
+            return ", 보너스 볼 일치";
+        }
+
+        return "";
     }
 }

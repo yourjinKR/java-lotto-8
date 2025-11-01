2cab5831e26a32b688b571fffc285ae446ed3d8b
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-01 21:29:28 +0900

    feat: 매칭점수와 보너스 매칭 여부가 일치하다면 매칭된 카운트 증가 메서드 추가

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -4,14 +4,22 @@ public class Winning {
     private final int score;
     private final boolean bonus;
     private final int prizeMoney;
+    private int matchedCount;
 
     public Winning(int score, boolean bonus, int prizeMoney) {
         this.score = score;
         this.bonus = bonus;
         this.prizeMoney = prizeMoney;
+        this.matchedCount = 0;
     }
 
     public boolean isMatched(int matchingScore, boolean isBonusMatched) {
         return (this.score == matchingScore && this.bonus == isBonusMatched);
+    }
+
+    public void countUpIfMatched(int matchingScore, boolean isBonusMatched) {
+        if (this.score == matchingScore && this.bonus == isBonusMatched) {
+            matchedCount++;
+        }
     }
 }
